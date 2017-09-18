2d8de6d3a316a100daafb33d85f386b278b65ed3
Eric Traub <eric@hedgeable.com>
2017-09-18 17:51:21 -0400

    updates

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+	componentDidMount() {
+		var posts = this.props.fetchPosts();
+		console.log('posts', posts);
+	}
+
 	render() {
 		return (
 			<div>
 				PostsIndex
-			</div>
+			</div>	
 		);
 	}
-}
+};
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
